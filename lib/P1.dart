6481ca0e857d019ad001db6fccdb06af57c2d259
Saumya <saumyaranjanjena19@gmail.com>
2023-09-22 23:37:39 +0530

    App change

--- lib/P1.dart
+++ lib/P1.dart
@@ -13,98 +13,143 @@ class P1 extends StatelessWidget {
           title: Text('AYUSH Pathyakram'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Select Course : ', style: GoogleFonts.openSans(fontSize: 20),),
-              SizedBox(height: 30,),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'P2');
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
-                  ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 8, vertical: 16), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: TextStyle(fontSize: 20), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.school), // Icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Ayurveda'), // Text
-                  ],
-                ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Center(
+              child: Image(
+                image: AssetImage('assets/images/bg_1.jpg'),
+                opacity: AlwaysStoppedAnimation(0.6),
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'P3');
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Select Course : ',
+                    style: GoogleFonts.openSans(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 8, vertical: 16), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: TextStyle(fontSize: 20), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.school), // Icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Unani'), // Text
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'P4');
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
+                  SizedBox(
+                    height: 30,
                   ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 8, vertical: 16), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: TextStyle(fontSize: 20), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.school), // Icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Siddha'), // Text
-                  ],
-                ),
-              ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'P2');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 20), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.school), // Icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Ayurveda (BAMS)'), // Text
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'P3');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 20), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.school), // Icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Unani (BUMS)'), // Text
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'P4');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 20), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.school), // Icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Siddha (BSMS)'), // Text
+                      ],
+                    ),
+                  ),
 
-            ],
-          ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'P4');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 20), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.school), // Icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Homeopathy (BHMS)'), // Text
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/P2.dart
+++ lib/P2.dart
@@ -13,119 +13,131 @@ class P2 extends StatelessWidget {
           title: Text('Select the Year'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'P5');
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
-                  ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.all(8), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: GoogleFonts.mooli(fontSize: 30), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.book), // Book icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Year 1'), // Text
-                  ],
-                ),
+        body: Stack(
+          children: [
+            Container(
+              child: Image(
+                image: AssetImage('assets/images/year_bg.jpg'),
+                opacity: AlwaysStoppedAnimation(0.7),
+                fit: BoxFit.fill,
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your onPressed logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
+              height: double.infinity,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'P5');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.all(8), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: GoogleFonts.mooli(fontSize: 30), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.book), // Book icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Year 1'), // Text
+                      ],
+                    ),
                   ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.all(8), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: TextStyle(fontSize: 30), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.book), // Book icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Year 2'), // Text
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your onPressed logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your onPressed logic here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.all(8), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 30), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.book), // Book icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Year 2'), // Text
+                      ],
+                    ),
                   ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.all(8), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: TextStyle(fontSize: 30), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.book), // Book icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Year 3'), // Text
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your onPressed logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Button shape
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your onPressed logic here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.all(8), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 30), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.book), // Book icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Year 3'), // Text
+                      ],
+                    ),
                   ),
-                  primary: Colors.blue, // Background color
-                  onPrimary: Colors.white, // Text color
-                  elevation: 5, // Elevation
-                  padding: EdgeInsets.all(8), // Padding
-                  shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
-                  textStyle: TextStyle(fontSize: 30), // Text style
-                  minimumSize: Size(150, 50), // Minimum button size
-                  side: BorderSide(color: Colors.blue.shade900, width: 2), // Border
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.book), // Book icon
-                    SizedBox(width: 8), // Add some space between icon and text
-                    Text('Year 4'), // Text
-                  ],
-                ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your onPressed logic here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black54, backgroundColor: Colors.white70, shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), // Button shape
+                      ), // Text color
+                      elevation: 5, // Elevation
+                      padding: EdgeInsets.all(8), // Padding
+                      shadowColor: Colors.blue.withOpacity(0.5), // Shadow color
+                      textStyle: TextStyle(fontSize: 30), // Text style
+                      minimumSize: Size(150, 50), // Minimum button size
+                      side: BorderSide(
+                          color: Colors.blue.shade900, width: 2), // Border
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.book), // Book icon
+                        SizedBox(
+                            width: 8), // Add some space between icon and text
+                        Text('Year 4'), // Text
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
